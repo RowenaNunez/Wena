9e8032225053ad858d6486eea1a237ab94e819a5
Michael Anthony Tura <turamichael166@gmail.com>
2026-03-24 19:03:42 +0800

    update

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/students', [StudentController::class, 'show']);
 Route::post('/students', [StudentController::class, 'create']);
 Route::delete('/students/{id}', [StudentController::class, 'delete']);
+Route::put('/students/{id}', [StudentController::class, 'update']);
