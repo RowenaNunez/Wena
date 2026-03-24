2e5a31ad3c2050f8093988ad3eebcd84fa7ecfe2
Michael Anthony Tura <turamichael166@gmail.com>
2026-03-24 20:24:54 +0800

    initial commit

--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/students', [StudentController::class, 'show']);
 Route::post('/students', [StudentController::class, 'create']);
 Route::delete('/students/{id}', [StudentController::class, 'delete']);
-Route::put('/students/{id}', [StudentController::class, 'update']);
+Route::put('/students/{id}', [StudentController::class, 'update'])
